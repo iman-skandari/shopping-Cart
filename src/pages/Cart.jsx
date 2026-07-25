c0eefce4cc7903399dbf791e60e26d18cbd35fe6
iman-skandari <imisk9919@gmail.com>
2026-07-25 12:40:47 +0330

    fix: display cart total correctly in Cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -24,16 +24,52 @@ const Cart = () => {
           </div>
           <div className="lg:col-span-1 p-8 bg-gray-900 rounded-2xl shadow-2xl border-1-4 sticky top-20 h-fit
           border border-gray-800  ">
-            <h3 className="">
-              <div>
-                <span className="">
-                $
+            <h3 className="text-3xl font-bold text-white mb-5 border-b border-gray-700 pb-3 flex items-center space-x-2">
+              <div className="flex justify-between">
+                <span className="w-6 h-6 text-orange-400">
+                  $
                 </span>
                 <span>
-                 Order Total
+                  Order Total
                 </span>
               </div>
             </h3>
+            <div className="space-y-4 text-gray-400 ">
+              <div className="flex justify-between text-xl ">
+                <span>
+                  SubTotal :
+                </span>
+                <span className="font-semibold text-white">
+                  ${cartTotal.toFixed(2)}
+                </span>
+              </div>
+              <div className="flex justify-between text-xl ">
+                <span>
+                  Shipping (Express):
+                </span>
+                <span className="font-semibold text-green-400">
+                  Free
+                </span>
+              </div>
+              <div className="flex justify-between pt-6 border-t border-gray-700 text-xl ">
+                <span className="text-2xl font-extrabold  text-white">
+                  EstimateTotal :
+                </span>
+                <span className="text-2xl font-extrabold text-orange-400">
+                  ${cartTotal.toFixed(2)}
+                </span>
+              </div>
+            </div>
+            <Link to={'/checkout'}
+              className="w-full mt-8 py-4 bg-orange-600 text-white font-extrabold text-xl rounded-full
+                shadow-lg shadow-orange-800/50 cursor-pointer hover:bg-orange-700 transition duration-300
+                flex items-center justify-center space-x-2 transform hover:ring-4 uppercase hover:ring-white-600 tracking-wider">
+              <Zap className="w-6 h-6" />
+              <span>PROCEED SECURELY</span>
+            </Link>
+            <p className="text-xs mt-4 text-gray-500  text-center"  >
+              All Transaction are encrypted and secure.
+            </p>
           </div>
         </div>
       </div>
