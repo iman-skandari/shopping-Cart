259aff14cad75dd98b32ab150cd3fec8a0d424db
iman-skandari <imisk9919@gmail.com>
2026-08-01 12:39:32 +0330

    feat: update checkout page layout and functionality

--- src/pages/CheckOut.jsx
+++ src/pages/CheckOut.jsx
@@ -1,20 +1,32 @@
 import { useState } from "react"
 import { useCart } from "../context/CartContext"
-import { Package, MapPin, Zap } from "lucide-react"
-import { Link } from "react-router-dom"
-import CartItem from "../components/CartItem/CartItem"
+import { Package, MapPin } from "lucide-react"
+
+import OrderConfirmation from "./OrderConfirmation"
 
 
 
 const CheckOut = () => {
+  const { cartTotal, clearCart, cart } = useCart()
   const [deliveryDetails, setDeliveryDetails] = useState({
     name: "",
     address: "",
     city: "",
     zip: ""
   })
+  const [isConfirm, setIsConfirm] = useState(false)
 
-const {cartTotal} = useCart()
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setDeliveryDetails(prev => ({ ...prev, [name]: value }))
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    clearCart();
+    setIsConfirm(true)
+  }
+  if (isConfirm) return <OrderConfirmation deliveryDetails={deliveryDetails} />
 
   return (
     <>
@@ -30,7 +42,7 @@ const {cartTotal} = useCart()
                 Shopping information
               </span>
             </h3>
-            <form className="space-y-6" >
+            <form className="space-y-6" onSubmit={handleSubmit}>
               {Object.keys(deliveryDetails).map((key) => (<div key={key}>
                 <label htmlFor={key} className="block text-sm font-semibold text-gray-300 capitalize mb-1 ">
                   {key === "zip" ? "Pin Code" : key}
@@ -40,20 +52,70 @@ const {cartTotal} = useCart()
                   name={key}
                   value={deliveryDetails[key]}
                   required
+                  onChange={handleChange}
                   className="mt-1 block w-full px-5 py-3 border border-gray-700 rounded-xl shadow-inner text-white bg-gray-800 
                 placeholder-gray-500 "
                 />
               </div>))}
               <div className="pt-6 ">
-              <button type="submit"
-              className="w-full py-4 bg-orange-600 text-white font-extrabold text-xl rounded-full
+                <button type="submit"
+                  className="w-full py-4 bg-orange-600 text-white font-extrabold text-xl rounded-full
                 shadow-lg shadow-orange-800/50 cursor-pointer hover:bg-orange-700 transition duration-300
                 flex items-center justify-center space-x-2 transform hover:ring-4 uppercase hover:ring-white-600 tracking-wider">
-              {/* <Zap className="w-6 h-6" /> */}
-              <span>$ PAY AND CONFIRM ORDER (${cartTotal.toFixed(2)})</span>
-            </button>
+                  {/* <Zap className="w-6 h-6" /> */}
+                  <span>$ PAY AND CONFIRM ORDER (${cartTotal.toFixed(2)})</span>
+                </button>
               </div>
             </form>
+          </div>
+          <div className="lg:col-span-1 p-8 bg-gray-900 rounded-2xl shadow-2xl border-1-4 sticky top-20 h-fit
+          border border-gray-800  ">
+            <h3 className="text-3xl font-bold text-white mb-5 border-b border-gray-700 pb-3 flex items-center space-x-2">
+              <Package className="w-6 h-6 text-orange-400" />
+              <span>
+                Summary
+              </span>
+            </h3>
+            <div className="space-y-4 text-gray-400">
+              {cart.map(item => <div key={item.id} className="flex justify-between text-base border-b border-gray-800
+               pb-2 ">
+                <span className="trucate text-gray-300 ">
+                  {item.name}
+                </span>
+                <span className="font-medium text-orange-300">
+                  $ {(item.price * item.quantity).toFixed(2)}
+                </span>
+              </div>)}
+
+
+              <div className="space-y-4 text-gray-400 ">
+                <div className="flex justify-between text-xl ">
+                  <span>
+                    SubTotal :
+                  </span>
+                  <span className="font-semibold text-white">
+                    ${cartTotal.toFixed(2)}
+                  </span>
+                </div>
+                <div className="flex justify-between text-xl ">
+                  <span>
+                    Shipping (Express):
+                  </span>
+                  <span className="font-semibold text-green-400">
+                    Free
+                  </span>
+                </div>
+                <div className="flex justify-between pt-6 border-t border-gray-700 text-xl ">
+                  <span className="text-2xl font-extrabold  text-white">
+                    EstimateTotal:
+                  </span>
+                  <span className="text-3xl font-extrabold text-orange-400">
+                     ${cartTotal.toFixed(2)}
+                  </span>
+                </div>
+              </div>
+            </div>
+           
           </div>
         </div>
       </div>
